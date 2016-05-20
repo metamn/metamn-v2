3601f4e2250ca629cea2c5878121db7cb9b9d36c
metamn <bartus.csongor@gmail.com>
2016-05-20 10:14:22 +0300

    Animation rethinked

--- code/project/about/about.js
+++ code/project/about/about.js
@@ -1,2 +1,3 @@
 var splitStringToChars = require('./../../framework/helpers/js/splitStringToChars.js');
-splitStringToChars('.about__text-2', '.about__chars');
+splitStringToChars('.about__text-1', '.about__chars-1');
+splitStringToChars('.about__text-2', '.about__chars-2');
