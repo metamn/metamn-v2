784877382e32a5d6ea9c442f9b0da7aecfe705da
metamn <bartus.csongor@gmail.com>
2020-01-24 14:49:28 +0200

    Replacing Typescript

---
+++ code/framework/design/typography/fonts/@assets/scripts/localfonts.js
@@ -0,0 +1,14 @@
+WebFontConfig = {
+  custom: {
+    families: ["Nimbus Sans", "Silkscreen"],
+    urls: ["/assets/styles/site.min.css"]
+  }
+};
+
+(function(d) {
+  var wf = d.createElement("script"),
+    s = d.scripts[0];
+  wf.src = "https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js";
+  wf.async = true;
+  s.parentNode.insertBefore(wf, s);
+})(document);
